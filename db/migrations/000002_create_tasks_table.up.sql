16c60764c4ebad09393dfc9d95c5dc32727d6a80
Benjamin Domey <domeybenjamin1@gmail.com>
2020-12-11 00:57:59 +0000

    added task and reviews tables

---
+++ db/migrations/000002_create_tasks_table.up.sql
@@ -0,0 +1,12 @@
+CREATE TYPE task_status_enum AS ENUM ('PENDING', 'DONE', 'FAILED');
+
+CREATE TABLE IF NOT EXISTS tasks(
+    id serial PRIMARY KEY,
+    created_by  serial NOT NULL REFERENCES users (id),
+    title VARCHAR(300) NOT NULL,
+    description TEXT,
+    deleted boolean DEFAULT FALSE,
+    status task_status_enum DEFAULT 'PENDING',
+    created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
+    updated_at TIMESTAMP NOT NULL DEFAULT current_timestamp
+);
---
+++ db/migrations/000003_create_reviews_table.up.sql
@@ -0,0 +1,11 @@
+CREATE TYPE review_status_enum AS ENUM ('PENDING','ACCEPT','REJECT');
+
+CREATE TABLE IF NOT EXISTS reviews(
+    id serial PRIMARY KEY,
+    task_id serial NOT NULL REFERENCES tasks (id),
+    reviewer serial NOT NULL REFERENCES users (id),
+    status review_status_enum DEFAULT 'PENDING',
+    reason TEXT,
+    created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
+    updated_at TIMESTAMP NOT NULL DEFAULT current_timestamp
+);
