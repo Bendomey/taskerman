1d9e55889eb6377e48c5a37b1ccadff051b16cdb
Benjamin Domey <domeybenjamin1@gmail.com>
2020-12-11 00:19:29 +0000

    add sql prepared statements for creating users and dropping users

---
+++ db/migrations/000001_create_users_table.up.sql
@@ -0,0 +1,11 @@
+-- CREATE TYPE user_type_enum AS ENUM ('ADMIN','USER','REVIEWER');
+
+CREATE TABLE IF NOT EXISTS users(
+   id serial PRIMARY KEY,
+   fullname VARCHAR (225) NOT NULL,
+   password VARCHAR (225) NOT NULL,
+   email VARCHAR (300) UNIQUE NOT NULL,
+   user_type user_type_enum DEFAULT 'USER',
+   created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
+   updated_at TIMESTAMP NOT NULL DEFAULT current_timestamp
+);
