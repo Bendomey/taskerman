eed27e91d02b86ff5f99d021a5cf3a2c182fbb2b
Benjamin Domey <domeybenjamin1@gmail.com>
2020-12-12 11:58:08 +0000

    Check if user exists and then save em

---
+++ utils/startup.go
@@ -0,0 +1,36 @@
+package utils
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/Bendomey/task-assignment/repository"
+)
+
+// SaveAdminInfo checks on startup if there is a super admin and then creates one when there isn't
+func SaveAdminInfo(repo repository.Repository) error {
+	//check if there are any users
+	rows, fetchRowErr := repo.GetAll(context.Background(), "select COUNT(*) from users")
+	if fetchRowErr != nil {
+		return fetchRowErr
+	}
+	var length int
+	for rows.Next() {
+		errOnScan := rows.Scan(&length)
+		if errOnScan != nil {
+			return errOnScan
+		}
+	}
+
+	if length == 0 {
+		log.Println("No users in db, creating one")
+		//create admin
+		_, err := repo.Insert(context.Background(), "insert into users (fullname,password,email,user_type) values($1,$2,$3,$4)", os.Getenv("ADMIN_NAME"), os.Getenv("ADMIN_PASSWORD"), os.Getenv("ADMIN_EMAIL"), os.Getenv("ADMIN_TYPE"))
+		if err != nil {
+			return err
+		}
+	}
+	log.Println("Taskerman booted successfully")
+	return nil
+}
